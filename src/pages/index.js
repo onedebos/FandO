942181c693a944ed5c6948f31f5898eb64238f16
Adebola <onedebos@gmail.com>
2020-06-03 23:02:55 +0100

    change font

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,16 +46,16 @@ const IndexPage = () => {
 
         <div className="mt-6 text-center">
           <ScrollIntoView selector="#ourList">
-            <Link className="block md:hidden font-bold note-text text-2xl revealList ">
+            <button className="block md:hidden font-bold note-text text-2xl revealList m-auto">
               See our List
-              <span className="font-semibold list text-xl">
-                <svg class="arrows">
+              {/* <span className="font-semibold list text-xl"> */}
+              {/* <svg class="arrows">
                   <path class="a1" d="M0 0 L30 32 L60 0"></path>
                   <path class="a2" d="M0 20 L30 52 L60 20"></path>
                   <path class="a3" d="M0 40 L30 72 L60 40"></path>
-                </svg>
-              </span>
-            </Link>
+                </svg> */}
+              {/* </span> */}
+            </button>
           </ScrollIntoView>
         </div>
       </div>
