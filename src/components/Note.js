e799e9cb1ad9fe2006c60e0ba5f612c1f424aa8e
Adebola <onedebos@gmail.com>
2020-06-16 10:18:24 +0100

    slide up inputs only

--- src/components/Note.js
+++ src/components/Note.js
@@ -43,28 +43,13 @@ const Note = () => {
         className="container m-auto p-8 md:p-0 max-w-md lg:max-w-lg pt-20"
       >
         <input type="hidden" name="form-name" value="contact" />
-        <div
-          className="text-center text-sm md:text-base"
-          data-sal="slide-up"
-          data-sal-delay="5"
-          data-sal-easing="ease"
-        >
+        <div className="text-center text-sm md:text-base">
           Want to tell us something?
         </div>
-        <div
-          className="text-center text-2xl font-semibold"
-          data-sal="slide-up"
-          data-sal-delay="5"
-          data-sal-easing="ease"
-        >
+        <div className="text-center text-2xl font-semibold">
           Leave us a message
         </div>
-        <div
-          className="mb-4 mt-4"
-          data-sal="slide-up"
-          data-sal-delay="5"
-          data-sal-easing="ease"
-        >
+        <div className="mb-4 mt-4">
           <label
             htmlFor="name"
             className="block text-gray-300 text-sm font-bold mb-2"
@@ -72,6 +57,9 @@ const Note = () => {
             Your Name:{" "}
           </label>
           <input
+            data-sal="slide-up"
+            data-sal-delay="5"
+            data-sal-easing="ease"
             onChange={({ target }) => setName(target.value)}
             type="text"
             name="name"
@@ -80,7 +68,7 @@ const Note = () => {
             className="bg-gray-400 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline font-medium"
           />
         </div>
-        <div data-sal="slide-up" data-sal-delay="5" data-sal-easing="ease">
+        <div>
           <label
             htmlFor="email"
             className="block text-gray-300 text-sm font-bold mb-2"
@@ -88,6 +76,9 @@ const Note = () => {
             Your Email:
           </label>
           <input
+            data-sal="slide-up"
+            data-sal-delay="5"
+            data-sal-easing="ease"
             type="email"
             name="email"
             value={email}
@@ -97,7 +88,7 @@ const Note = () => {
           />
         </div>
 
-        <div data-sal="slide-up" data-sal-delay="5" data-sal-easing="ease">
+        <div>
           <label
             htmlFor="message"
             className="block text-gray-300 text-sm font-bold mb-2 mt-4"
@@ -105,6 +96,9 @@ const Note = () => {
             Message:
           </label>
           <textarea
+            data-sal="slide-up"
+            data-sal-delay="5"
+            data-sal-easing="ease"
             name="message"
             placeholder="say something nice...."
             rows="5"
