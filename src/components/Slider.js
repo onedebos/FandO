e8974bea69a3a97b89b74fbbb4c83565752964ff
Adebola <onedebos@gmail.com>
2020-06-16 10:55:34 +0100

    add message button

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -14,8 +14,8 @@ const Slider = ({ info }) => {
         }}
       >
         <div className="text-white md:grid ml-4 p-8 md:p-0 md:ml-10 lg:ml-0 md:grid-cols-2 pt-12 lg:pt-20">
-          <div className="col-span-1 mt-8 md:mt-20 ">
-            <h1 className="text-3xl font-bold">
+          <div className="col-span-1 mt-10 md:mt-20 ">
+            <h1 className="text-3xl font-bold leading-tight md:leading-normal">
               We said <span className="text-yellow-gold">Yes</span> to each
               other. <br />
               Now, we're so thrilled you said{" "}
@@ -32,7 +32,7 @@ const Slider = ({ info }) => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="mt-2 sm:mt-8 bg-transparent hover:bg-yellow-500 text-white font-semibold hover:text-white py-2 px-8 sm:px-2 lg:px-4 border-2 border-yellow-gold hover:border-transparent rounded-lg w-full sm:w-auto">
+              <button className="mt-1 sm:mt-8 bg-transparent hover:bg-yellow-500 text-white font-semibold hover:text-white py-2 px-8 sm:px-2 lg:px-4 border-2 border-yellow-gold hover:border-transparent rounded-lg w-full sm:w-auto">
                 Join virtually
               </button>
             </a>
@@ -40,6 +40,11 @@ const Slider = ({ info }) => {
             <button className="bg-transparent border-yellow-gold hover:bg-yellow-500 text-white font-semibold hover:text-white py-2 px-4 border-2 border-yellow-500 hover:border-transparent rounded-lg md:ml-0 mt-2 md:mt-4 md:ml-4 w-full sm:w-auto ">
               <ScrollIntoView selector="#ourList">
                 See our gifts register
+              </ScrollIntoView>
+            </button>
+            <button className="bg-transparent border-yellow-gold hover:bg-yellow-500 text-white font-semibold hover:text-white py-2 px-4 border-2 border-yellow-500 hover:border-transparent rounded-lg md:ml-0 mt-2 md:mt-4 w-full sm:w-auto ">
+              <ScrollIntoView selector="#message">
+                Leave us a message
               </ScrollIntoView>
             </button>
           </div>
