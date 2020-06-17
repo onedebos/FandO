f346acd5a08205383dfaf28741071bacca1ddc52
Adebola <onedebos@gmail.com>
2020-06-17 23:57:31 +0100

    use higher res image in footer

--- src/components/GiftRegistry.js
+++ src/components/GiftRegistry.js
@@ -2,13 +2,15 @@ import React from "react"
 
 const GiftRegistry = () => {
   return (
-    <div className="text-center p-12 thank-you-hero" id="ourList">
-      <h1 className="text-xl md:text-2xl font-semibold">
-        Here's a wishlist of some of our favorite things.
-      </h1>
-      <p className="md:font-medium text-sm md:text-md">
-        Thanking you in advance for your goodwill.
-      </p>
+    <div id="ourList" className="">
+      <div className="text-center p-12 thank-you-hero">
+        <h1 className="text-xl md:text-2xl font-semibold mt-10">
+          Here's a wishlist of some of our favorite things.
+        </h1>
+        <p className="md:font-medium text-sm md:text-md">
+          Thanking you in advance for your goodwill.
+        </p>
+      </div>
     </div>
   )
 }
